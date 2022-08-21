76be272c896dac6c3d18c0240dcb6deb6b8056d9
Javier Bueno <javitempab@gmail.com>
2022-08-21 10:44:05 +0200

    link mocked GraphQL data

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,11 +5,15 @@ import { ApolloServer } from 'apollo-server-express';
 import schema from './graphql/schema';
 import casual from 'casual';  // fake data generator
 
+// linking the mocked data
+let postsIds: string[] = [];
+let usersIds: string[] = [];
+
 
 // mocking graphql api with casual
 const mocks = {
     User: () => ({
-      id: casual.uuid,
+      id: () => { let uuid = casual.uuid; usersIds.push(uuid); return uuid },
       fullName: casual.full_name,
       bio: casual.text,
       email: casual.email,
@@ -21,7 +25,8 @@ const mocks = {
     }),
 
     Post: () => ({
-      id: casual.uuid,
+      id: () => { let uuid = casual.uuid; postsIds.push(uuid); return uuid },
+      author: casual.random_element(usersIds),
       text: casual.text,
       image: 'https://picsum.photos/seed/picsum/350/350',
       commentsCount: () => casual.integer(0,100),
@@ -33,15 +38,16 @@ const mocks = {
     
     Comment: () => ({
       id: casual.uuid,
+      author: casual.random_element(usersIds),
       comment: casual.text,
-      post: casual.uuid,
+      post: casual.random_element(postsIds),
       createdAt: () => casual.date()
     }),
     
     Like: () => ({
       id: casual.uuid,
-      user: casual.uuid,
-      post: casual.uuid
+      user: casual.random_element(usersIds),
+      post: casual.random_element(postsIds)
     }),
     
     Query: () =>({
